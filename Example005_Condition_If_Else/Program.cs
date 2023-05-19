b186e967f856c925d5affa6b9b697f7230d2af6d
Ivan <ivanchul@yandex.ru>
2023-05-19 12:27:47 +0400

    added Example005_Condition_If_Else

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter User's name: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "ivan") {
+    Console.WriteLine("Welcome, your Majesty");
+}
+else {
+    Console.WriteLine("Hello, lackey");
+}
