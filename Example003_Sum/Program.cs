d5341535fc3f380db0f821529d1751f12bb394a9
Ivan <ivanchul@yandex.ru>
2023-05-19 12:06:59 +0400

    Added random in Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,8 @@
-﻿int number1 = 3;
-int number2 = 5;
-Console.Write (number1 + number2);
+﻿int number1 = new Random().Next(1, 10); // 1,2,3,4,5,6,7,8,9
+int number2 = new Random().Next(1, 10);
+Console.Write("Number 1 = ");
+Console.WriteLine(number1);
+Console.Write("Number 2 = ");
+Console.WriteLine(number2);
+Console.Write("Sum = ");
+Console.WriteLine (number1 + number2);
