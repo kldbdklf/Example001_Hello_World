a3c60abd232febfa1657bcb0d040099d7b953c22
Ivan <ivanchul@yandex.ru>
2023-05-19 12:41:50 +0400

    add Example006_Max

---
+++ Example006_Max/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = 1, b = 2, c = 6, d = 8, e = 4, max = a;
+if (max < a ) max = a;
+if (max < b ) max = b;
+if (max < c ) max = c;
+if (max < d ) max = d;
+if (max < e ) max = e;
+Console.Write("Max = ");
+Console.WriteLine(max);
